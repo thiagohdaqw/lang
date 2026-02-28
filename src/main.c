e509bd3e87bd805a96742764923a2b055c97169a
Thiago Paiva <thiagohdaqw@gmail.com>
2026-02-28 20:53:32 -0300

    Added initial parser

--- src/main.c
+++ src/main.c
@@ -1,47 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "arena.h"
 #include "lexer.h"
+#include "parser.h"
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        fprintf(stderr, "[MAIN]: No file path specified\n");
-        fprintf(stderr, "[MAIN]: Usage: %s <file_path>\n", argv[0]);
-        return 1;
-    }
-    
-    
-    Lexer lexer = {0};
-    if (!lexer_init(&lexer, argv[1])) {
-        return 1;
-    }
+  if (argc < 2) {
+    fprintf(stderr, "[MAIN]: No file path specified\n");
+    fprintf(stderr, "[MAIN]: Usage: %s <file_path>\n", argv[0]);
+    return 1;
+  }
 
-    while (lexer_next_token(&lexer)) {
-        printf("%s:%d:%d =>\t", lexer.path, lexer.reader.row + 1, lexer.reader.col);
-        switch (lexer.token.type)
-        {
-        case T_STRING:
-            printf("STRING(%s)\n", lexer.token.string_value);
-            break;
-        case T_CHAR:
-            printf("CHAR(%c)\n", lexer.token.char_value);
-            break;
-        case T_LITERAL:
-            printf("LITERAL(%c)\n", lexer.token.literal_value);
-            break;
-        case T_INT:
-            printf("INT(%d)\n", lexer.token.long_value);
-            break;
-        case T_IDENTIFIER:
-            printf("IDENTIFIER(%s)\n", lexer.token.identifier_value);
-            break;
-        case T_DOUBLE:
-            printf("DOUBLE(%lf)\n", lexer.token.double_value);
-            break;
-        default:
-            printf("NOT_IMPLEMENTED(%d)\n", lexer.token.type);
-            break;
-        }
+  Lexer lexer = {0};
+  if (!lexer_create(&lexer, argv[1])) {
+    return 1;
+  }
+
+  Arena arena = arena_create(4 * 1024);
+
+  while (1) {
+    ExprNode* expr = parser_expression(&lexer, &arena);
+    if (expr == NULL) {
+      if (lexer_is_eof(&lexer)) break;
+      continue;
     }
-    printf("EOF\n");
-    return 0;
+    parser_print_expression(expr);
+
+    printf("Result: ");
+    ExprNode* result = parser_eval_expr(expr, &arena);
+    assert(result && "Unexpected result null");
+    parser_print_expression(result);
+  }
+
+  arena_destroy(&arena);
+
+  return 0;
 }
